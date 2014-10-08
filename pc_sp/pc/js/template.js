8903892499774f1f96e4a04d0257460f13578016
kishiya <kishiya@vitalify.jp>
2014-10-08 11:55:45 +0900

    レイアウトの調整

--- pc_sp/pc/js/template.js
+++ pc_sp/pc/js/template.js
@@ -8,22 +8,22 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1, helper;
-  buffer += "\r\n<div class=\"js-modal"
+  buffer += "\n<div class=\"js-modal"
     + escapeExpression(((stack1 = (data == null || data === false ? data : data.index)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + " js-modal\">\r\n	<h3 class=\"modal-title\">";
+    + " js-modal\">\n	<h3 class=\"modal-title\">";
   if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "</h3>\r\n	<div class=\"clearfix\">\r\n		<div class=\"modal-left\">\r\n			<img src=\"";
+    + "</h3>\n	<div class=\"clearfix\">\n		<div class=\"modal-left\">\n			<img src=\"";
   if (helper = helpers.src) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.src); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "\" alt=\"\" width=\"300\">\r\n		</div>\r\n	</div>\r\n</div>\r\n";
+    + "\" alt=\"\" width=\"300\">\n		</div>\n	</div>\n</div>\n";
   return buffer;
   }
 
   stack1 = helpers.each.call(depth0, (depth0 && depth0.modal_data), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\r\n";
+  buffer += "\n";
   return buffer;
   });
